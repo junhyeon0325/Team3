a112e1f1299bcc8bafc491559978ff5c199dea70
junhyeon0325 <jjh990325@gmail.com>
2025-08-13 01:18:06 +0900

    20250813 0117 tiles제작

---
+++ Team3MiddleProject/src/main/java/com/yedam/common/FrontController.java
@@ -0,0 +1,37 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.control.MainControl;
+
+public class FrontController extends HttpServlet{
+	Map<String, Control> map;
+	
+	// 생성자
+	public FrontController() {
+		map = new HashMap<String, Control>();
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/main.do", new MainControl());
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();
+		String path = uri.substring(context.length());
+
+		Control sub = map.get(path);
+		sub.execute(req, resp);
+	}
+}
---
+++ Team3MiddleProject/src/main/java/com/yedam/control/MainControl.java
@@ -0,0 +1,18 @@
+package com.yedam.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.common.Control;
+
+public class MainControl implements Control{
+	
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//req.getRequestDispatcher("index.jsp").forward(req, resp);
+		req.getRequestDispatcher("product/productMain.tiles").forward(req, resp);
+	}
+}
