bb42922f1361ec8aff251d5e379512b13c0b2bb0
junhyeon0325 <jjh990325@gmail.com>
2025-08-14 01:49:59 +0900

    장준현_20250814 0149_control수정

--- Team3MiddleProject/src/main/java/com/yedam/control/CartListControl.java
+++ Team3MiddleProject/src/main/java/com/yedam/control/CartListControl.java
@@ -7,15 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.common.Control;
-import com.yedam.service.CartService;
 
 public class CartListControl implements Control {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String mno = req.getParameter("mno");
-		String pno = req.getParameter("pno");
-		String ppcs = req.getParameter("ppcs");
+		
+		
+		
 		
 		ppcs = "5";	// 테스트
 		
--- Team3MiddleProject/src/main/java/com/yedam/service/CartService.java
+++ Team3MiddleProject/src/main/java/com/yedam/service/CartService.java
@@ -3,5 +3,5 @@ package com.yedam.service;
 import com.yedam.vo.MemberVO;
 
 public class CartService {
-	public MemberVO searchMember(String mid);	// 회원조회
+	//public MemberVO searchMember(String mid);	// 회원조회
 }
