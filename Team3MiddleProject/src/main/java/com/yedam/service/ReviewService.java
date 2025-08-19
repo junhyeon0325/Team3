1def2da7dd8dd7ec26e0024d41c101b9aebbcecf
woong254 <wjdwldnd254@naver.com>
2025-08-19 22:59:56 +0900

    0819 homework

--- Team3MiddleProject/src/main/java/com/yedam/service/ReviewService.java
+++ Team3MiddleProject/src/main/java/com/yedam/service/ReviewService.java
@@ -1,8 +1,12 @@
 package com.yedam.service;
 
+import java.util.List;
+
 import com.yedam.vo.ReviewVO;
 
 public interface ReviewService {
 	boolean addReview(ReviewVO review);
+	List<ReviewVO> reviewList(int productNo, int page);
+	int countReview(int productNo);
 }
 //실행
--- Team3MiddleProject/src/main/java/com/yedam/service/ReviewServiceImpl.java
+++ Team3MiddleProject/src/main/java/com/yedam/service/ReviewServiceImpl.java
@@ -1,5 +1,9 @@
 package com.yedam.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.yedam.common.DBUtil;
@@ -10,6 +14,7 @@ public class ReviewServiceImpl implements ReviewService {
 	SqlSession sqlSession = DBUtil.getInstance().openSession();
 	ReviewMapper mapper = sqlSession.getMapper(ReviewMapper.class);
 	
+	//권한체크 및 등록
 	public boolean addReview(ReviewVO review) {
 		int r = mapper.authorityReview(review);
 		if (r > 0) {
@@ -19,4 +24,23 @@ public class ReviewServiceImpl implements ReviewService {
 		}
 		return false;
 	}
+	
+	//페이징 리뷰목록
+	@Override
+	public List<ReviewVO> reviewList(int productNo, int page) {
+		int start = (page - 1)* 5 + 1;
+		int end = start + 4;
+		Map<String, Object> param = new HashMap<>();
+		param.put("productNo", productNo);
+		param.put("start", start);
+		param.put("end", end);
+		return mapper.selectReviewList(param);
+	}
+	
+	//리뷰 카운트
+	@Override
+	public int countReview(int productNo) {
+		return mapper.countReview(productNo);
+	}
+	
 }
