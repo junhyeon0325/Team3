d610fbb6ab97359aeaeec4e97e0d08d0e03c8d28
junhyeon0325 <jjh990325@gmail.com>
2025-08-20 13:42:59 +0900

    1

--- Team3MiddleProject/src/main/webapp/sql/0813.sql
+++ Team3MiddleProject/src/main/webapp/sql/0813.sql
@@ -1,3 +1,86 @@
+create table tbl_product(                   
+    product_no number primary key,
+    product_name varchar2(100) not null,
+    product_seller varchar2(100) not null,
+    product_price number not null,
+    product_score number, -- not null
+    product_image varchar2(100) not null,
+    expiration_date date not null,
+    product_about varchar2(500),
+    maincategory varchar2(100) not null
+);
+create table tbl_review(
+    review_no number primary key,
+    review_content varchar2(1000) not null,
+    review_image varchar2(100),
+    --review_score number,  tbl_product占쏙옙 product_score 占쌍는듸옙 占쏘떻占쏙옙 占쏙옙占쏙옙占쏙옙占? 확占쏙옙
+    review_date date default sysdate,             --default 占쏙옙 sysdate
+    product_no number not null,
+    order_no number not null,
+    
+    constraint fk_review_product
+    foreign key(product_no)
+    references tbl_product(product_no)
+    on delete cascade,
+    
+    constraint fk_review_order
+    foreign key(order_no)
+    references tbl_order(order_no)
+    on delete cascade
+);
+alter table tbl_product modify maincategory invisible;
+alter table tbl_product modify maincategory visible;
+select *
+from tbl_product;
+drop table tbl_reply;
+delete from tbl_order;
+select *
+from tbl_cart;
+
+select sequence_name
+from user_sequences;
+drop sequence product_seq;
+select product_seq.currval from dual;
+SELECT
+    constraint_name,
+    constraint_type,
+    table_name,
+    column_name
+FROM
+    user_cons_columns
+WHERE
+    constraint_name = 'SYS_C008462';
+CREATE SEQUENCE product_seq START WITH 1 INCREMENT BY 1;
+INSERT INTO tbl_product values(product_seq.nextval, '가자미', 'Team3', 12000, 4, 'product-IMG/가자미.jpg', TO_DATE('2025-08-25', 'YYYY-MM-DD'), '갓 잡은 신선한 가자미!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '갈치', 'Team3', 18000, 5, 'product-IMG/갈치.jpg', TO_DATE('2025-09-10', 'YYYY-MM-DD'), '밥도둑 갈치!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '감자', 'Team3', 5000, 3, 'product-IMG/감자.jpg', TO_DATE('2025-10-01', 'YYYY-MM-DD'), '강원도 햇감자!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '고구마', 'Team3', 6000, 4, 'product-IMG/고구마.jpg', TO_DATE('2025-10-15', 'YYYY-MM-DD'), '달콤한 꿀고구마!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '고등어', 'Team3', 9000, 5, 'product-IMG/고등어.jpg', TO_DATE('2025-09-05', 'YYYY-MM-DD'), '노르웨이산 고등어!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '간마늘', 'Team3', 3000, 4, 'product-IMG/간마늘.jpg', TO_DATE('2025-11-20', 'YYYY-MM-DD'), '요리의 필수품 간마늘!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '깻잎', 'Team3', 2000, 5, 'product-IMG/깻잎.jpg', TO_DATE('2025-09-30', 'YYYY-MM-DD'), '향긋한 깻잎!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '낙지', 'Team3', 11000, 5, 'product-IMG/낙지.jpg', TO_DATE('2025-10-25', 'YYYY-MM-DD'), '바다의 보양식 낙지!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '냉동과일', 'Team3', 8000, 4, 'product-IMG/냉동과일.jpg', TO_DATE('2026-01-10', 'YYYY-MM-DD'), '달콤 시원한 냉동과일!', '과일');
+INSERT INTO tbl_product values(product_seq.nextval, '달걀', 'Team3', 7000, 5, 'product-IMG/달걀.jpg', TO_DATE('2025-09-01', 'YYYY-MM-DD'), '신선한 유정란!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '닭', 'Team3', 10000, 4, 'product-IMG/닭.jpg', TO_DATE('2025-08-28', 'YYYY-MM-DD'), '신선한 닭고기!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '당근', 'Team3', 4000, 3, 'product-IMG/당근.jpg', TO_DATE('2025-11-05', 'YYYY-MM-DD'), '싱싱한 유기농 당근!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '대파', 'Team3', 2500, 4, 'product-IMG/대파.jpg', TO_DATE('2025-10-18', 'YYYY-MM-DD'), '국내산 대파!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '등심', 'Team3', 25000, 5, 'product-IMG/등심.jpg', TO_DATE('2025-12-01', 'YYYY-MM-DD'), '육즙 가득한 소고기 등심!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '마늘', 'Team3', 6000, 5, 'product-IMG/마늘.jpg', TO_DATE('2025-11-15', 'YYYY-MM-DD'), '통통한 의성 마늘!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '방어', 'Team3', 20000, 5, 'product-IMG/방어.jpg', TO_DATE('2025-12-25', 'YYYY-MM-DD'), '제철 맞은 싱싱한 방어!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '브로콜리', 'Team3', 4500, 4, 'product-IMG/브로콜리.jpg', TO_DATE('2025-10-05', 'YYYY-MM-DD'), '영양 가득 브로콜리!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '살치살', 'Team3', 28000, 5, 'product-IMG/살치살.jpg', TO_DATE('2025-12-10', 'YYYY-MM-DD'), '입에서 살살 녹는 살치살!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '안심', 'Team3', 30000, 5, 'product-IMG/안심.jpg', TO_DATE('2025-12-05', 'YYYY-MM-DD'), '부드러운 소고기 안심!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '양념소불고기', 'Team3', 18000, 4, 'product-IMG/양념소불고기.jpg', TO_DATE('2025-11-25', 'YYYY-MM-DD'), '간편하게 즐기는 양념 소불고기!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '양배추', 'Team3', 3500, 4, 'product-IMG/양배추.jpg', TO_DATE('2025-11-01', 'YYYY-MM-DD'), '아삭한 양배추!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '양파', 'Team3', 3000, 4, 'product-IMG/양파.jpg', TO_DATE('2025-10-22', 'YYYY-MM-DD'), '어디에나 잘 어울리는 양파!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '연어필렛', 'Team3', 16000, 5, 'product-IMG/연어필렛.jpg', TO_DATE('2025-09-15', 'YYYY-MM-DD'), '신선한 노르웨이산 연어필렛!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '오이', 'Team3', 2500, 3, 'product-IMG/오이.jpg', TO_DATE('2025-09-20', 'YYYY-MM-DD'), '아삭하고 시원한 오이!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '오징어', 'Team3', 13000, 4, 'product-IMG/오징어.jpg', TO_DATE('2025-10-08', 'YYYY-MM-DD'), '쫄깃한 동해안 오징어!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '와규', 'Team3', 45000, 5, 'product-IMG/와규.jpg', TO_DATE('2026-01-20', 'YYYY-MM-DD'), '고급스러운 와규!', '정육');
+INSERT INTO tbl_product values(product_seq.nextval, '키조개', 'Team3', 10000, 4, 'product-IMG/키조개.jpg', TO_DATE('2025-11-10', 'YYYY-MM-DD'), '쫄깃한 키조개 관자!', '수산');
+INSERT INTO tbl_product values(product_seq.nextval, '파프리카', 'Team3', 5000, 4, 'product-IMG/파프리카.jpg', TO_DATE('2025-11-18', 'YYYY-MM-DD'), '알록달록 파프리카!', '채소');
+INSERT INTO tbl_product values(product_seq.nextval, '표고버섯', 'Team3', 7000, 5, 'product-IMG/표고버섯.jpg', TO_DATE('2025-12-15', 'YYYY-MM-DD'), '향긋한 표고버섯!', '채소');  
+
 -- update 적립금
 update tbl_member
 set point = 10000;
@@ -13,7 +96,7 @@ select count(*)
         
 select *
 from tbl_order_items;
-
+delete FROM tbl_order_items;
 ALTER TABLE TBL_ORDER DROP COLUMN PRODUCT_NO;
 drop table tbl_order_items;
 
@@ -130,6 +213,7 @@ values (2,0.1,2);
 insert into tbl_event
 values (3,0.2,3);
 
+delete from tbl_event;
 -- 할인 이벤트 상품 테이블 삭제
 DROP table tbl_event;
 
@@ -244,8 +328,8 @@ create table tbl_product(                   -- 수정
     product_name varchar2(100) not null,
     product_seller varchar2(100) not null,
     product_price number not null,
-    --maincategory varchar2(100) not null,
-    subcategory varchar2(100) not null,
+    maincategory varchar2(100) not null,
+    --subcategory varchar2(100) not null,
     product_score number, -- not null
     product_image varchar2(100) not null,
     product_about varchar2(500),
